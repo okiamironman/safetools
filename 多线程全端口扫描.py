1fca4f74b59408b305644b0014fcb0b21172b346
okiamironman <103590388+okiamironman@users.noreply.github.com>
2023-02-23 00:03:01 +0800

    Add files via upload

---
+++ 多线程全端口扫描.py
@@ -0,0 +1,27 @@
+
+import socket,sys,threading,queue
+
+# 多线程全端口扫描，参数模式，自定义ip,线程数
+
+def full_port_scan(ip):
+    while not q.empty():
+        port=q.get()
+        s=socket.socket()
+        try:
+            s.connect((ip,port))
+            print(ip+':'+str(port)+':open')
+        except Exception as e:
+            print(ip+':'+str(port)+':close')
+            # pass
+        finally:
+            s.close()
+if __name__ == '__main__':
+    ip=sys.argv[1]
+    th_nums=sys.argv[2]
+
+    q=queue.Queue()
+    for port in range(1,65536):
+        q.put(port)
+    for th_num in range(int(th_nums)):
+        t=threading.Thread(target=full_port_scan,args=(ip,))
+        t.start()
